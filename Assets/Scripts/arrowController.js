2b00b13b92246dbd46c8d2105f905eda16154942
jsettlem <jsettlem@cs.unc.edu>
2014-09-06 13:38:59 -0400

    Arrows that point!

---
+++ Assets/Scripts/arrowController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var pointVector : Vector2;
+private var length : float;
+private var magicNumber1 : float = 0.100541;
+
+function Start () {
+	pointVector = Vector2(0,0);
+}
+
+function Update () {
+	length = pointVector.magnitude;
+	transform.rotation.eulerAngles.z = -1 * Mathf.Atan2(pointVector.x, pointVector.y) * Mathf.Rad2Deg;
+	transform.GetChild(1).transform.localScale.y = length;
+	transform.GetChild(0).transform.localPosition.y = magicNumber1 + length;
+}
--- Assets/Scripts/shipController.js
+++ Assets/Scripts/shipController.js
@@ -12,10 +12,16 @@ function Start () {
 function FixedUpdate () {
 	rigidbody.velocity += Vector3(x_accel, y_accel, 0) * Time.deltaTime;
 	rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, max_velocity);
+	
 	if (x_accel == 0 && Mathf.Abs(rigidbody.velocity.x) < deadZone) {
 		rigidbody.velocity.x = 0;
 	}
 	if (y_accel == 0 && Mathf.Abs(rigidbody.velocity.y) < deadZone) {
 		rigidbody.velocity.y = 0;
 	}
+	
+}
+
+function onGUI() {
+	
 }
