d5a89ecf8f5841c2bbbcfc5a0a817cdcdec0b7cb
Adam Aji <adam.m.aji@lmco.com>
2014-09-07 09:16:30 -0400

    Separate accel arrow into x and y components

--- Assets/Scripts/shipController.js
+++ Assets/Scripts/shipController.js
@@ -5,7 +5,8 @@ public var y_accel : float;
 public var max_velocity : float;
 public var deadZone : float;
 public var vArrow : arrowController;
-public var aArrow : arrowController;
+public var axArrow : arrowController;
+public var ayArrow : arrowController;
 
 function Start () {
 
@@ -25,7 +26,8 @@ function FixedUpdate () {
 	transform.rotation.eulerAngles.z = Mathf.Atan2(rigidbody.velocity.y, rigidbody.velocity.x) * Mathf.Rad2Deg;
 	
 	vArrow.pointVector = Vector2(rigidbody.velocity.x, rigidbody.velocity.y) * 0.5;
-	aArrow.pointVector = Vector2(x_accel, y_accel) * 0.5;
+	axArrow.pointVector = Vector2(x_accel, 0 ) * 0.5;
+	ayArrow.pointVector = Vector2(0, y_accel) *0.5;
 }
 
 function OnCollisionEnter(collision : Collision){
